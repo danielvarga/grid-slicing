d076e1018347473a6d378860c59198eb8eb16d1b
Daniel Varga <daniel.varga@prezi.com>
2021-11-03 23:02:56 +0100

    dual task

---
+++ dual_set_system.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+
+n, outfilename = sys.argv[1:]
+n = int(n)
+assert n % 2 == 0
+m = n // 2
+
+
+def line(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    a = (y2-y1)/(x2-x1)
+    # a * x1 + b = y1
+    b = y1 - a * x1
+    return a, b
+
+
+def random_line(n):
+    s = np.zeros((n, n), dtype=np.uint8)
+    p, q = np.random.uniform(low=1/2, high=n - 1/2, size=2) # x_0 y_0 in the paper
+    a, b = line((0, p), (m, q))
+    for i in range(m):
+        j = a * (i + 0.5) + b
+        j = int(np.round(j))
+        s[i, j] = 1
+    a, b = line((m, q), (n, n-p))
+    for i in range(m, n):
+        j = a * (i + 0.5) + b
+        j = int(np.round(j))
+        s[i, j] = 1
+    return s
+
+
+def sampling_collect_lines(n, samples, patience):
+    ss = set()
+    total_attempts = 0
+    attempts = 0
+    while True:
+        result = random_line(n)
+        total_attempts += 1
+        attempts += 1
+        result_tup = tuple(map(tuple, result))
+        before = len(ss)
+        ss.add(result_tup)
+        if len(ss) > before:
+            attempts = 0
+        if attempts >= patience:
+            break
+        if total_attempts >= samples:
+            break
+        if total_attempts % 1000 == 0:
+            print(total_attempts, attempts, len(ss))
+    return np.array(list(ss)).astype(np.uint8)
+
+
+s = random_line(n)
+print(s)
+
+collected_lines = sampling_collect_lines(n, samples=10000, patience=1000)
+
+np.save(open(outfilename, "wb"), collected_lines)
---
+++ vis_dual.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+a = np.load(open(sys.argv[1], "rb"))
+
+
+print("symmetrizing")
+a = (a + a[:, ::-1]) / 2
+a = (a + a[::-1, :]) / 2
+
+
+plt.imshow(-a, cmap='gist_gray')
+plt.show()
+
+print("sum of Lagrangian", a.sum())
+
+n, m = a.shape
+
+def mygrid(shape):
+    n, m = shape
+    xi = np.arange(n)
+    yi = np.arange(m)
+    xx, yy = np.meshgrid(xi, yi)
+    # g = np.stack([np.arange(n + 1)[:, np.newaxis], np.arange(m + 1)[np.newaxis, :]], axis=-1)
+    g = np.stack([xx, yy], axis=-1)
+    return g
+
+g = mygrid(a.shape)
+
+x = g[:, :, 0] / (n - 1) * 2 - 1
+y = g[:, :, 1] / (m - 1) * 2 - 1
+
+
+X = np.abs(x.flatten())
+Y = np.abs(np.abs(y.flatten()) - 0.5)
+
+# https://stackoverflow.com/a/33966967/383313
+
+# P = np.array([X*0+1, X, Y, X**2, X**2*Y, X**2*Y**2, Y**2, X*Y**2, X*Y]).T
+
+degree = 3
+
+monomials = []
+for xd in range(degree + 1):
+    for yd in range(degree + 1):
+        monomials.append(np.abs(X) ** xd * np.abs(Y) ** yd)
+        # monomials.append(X ** xd * Y ** yd)
+P = np.array(monomials).T
+
+Q = a.flatten()
+print(P.shape, Q.shape)
+
+coeff, r, rank, s = np.linalg.lstsq(P, Q)
+print("coeff", coeff, rank, s)
+
+prediction = P.dot(coeff).reshape((n, m))
+plt.imshow(-prediction, cmap='gray')
+plt.show()
+
+
+coeff[np.abs(coeff) < 1e-3] = 0
+print(coeff)
+prediction = P.dot(coeff).reshape((n, m))
+plt.imshow(prediction)
+plt.show()
+
+exit()
+
+dist = np.sqrt(x * x + y * y)
+
+plt.scatter(dist.flatten(), a.flatten(), c=np.minimum(np.abs(x), np.abs(y)).flatten())
+plt.title("distance from origin vs value, empirical.\ncolor:distance from axes")
+plt.show()
+
+plt.scatter(dist.flatten(), prediction.flatten(), c=np.minimum(np.abs(x), np.abs(y)).flatten())
+plt.title("distance from origin vs value, analytical approximation.\ncolor:distance from axes")
+plt.show()
+
+l1_dist = np.abs(x) + np.abs(y)
+
+plt.scatter(l1_dist.flatten(), a.flatten(), c=np.minimum(np.abs(x), np.abs(y)).flatten())
+plt.title("L1 distance from origin vs value, empirical.\ncolor:distance from axes")
+plt.show()
